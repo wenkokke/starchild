839621060a6a35f2c55471abf00c164f94676684
leina0 <Daniel.Kienitz@web.de>
2020-03-13 13:22:50 +0000

    Added export

--- train_MNIST_PCA_64_ReLU_49_Softmax_10.py
+++ train_MNIST_PCA_64_ReLU_49_Softmax_10.py
@@ -16,17 +16,28 @@ train_images = train_images.reshape((-1, 784)) / 255
 test_images = test_images.reshape((-1, 784)) / 255
 
 
+# ideal = {
+#     '0':[4655, 5884, 46535], 
+#     '1':[7748, 48044, 5824], 
+#     '2':[1584, 31258, 20242], 
+#     '3':[26364, 19247, 25631], 
+#     '4':[26172, 32832, 39492], 
+#     '5':[19175, 32566, 25411], 
+#     '6':[58755, 41500, 45410], 
+#     '7':[9724, 59665, 6915], 
+#     '8':[35484, 41132, 50302], 
+#     '9':[19085, 3481, 25375]}
 ideal = {
-    '0':[4655, 5884, 46535], 
-    '1':[7748, 48044, 5824], 
-    '2':[1584, 31258, 20242], 
-    '3':[26364, 19247, 25631], 
-    '4':[26172, 32832, 39492], 
-    '5':[19175, 32566, 25411], 
-    '6':[58755, 41500, 45410], 
-    '7':[9724, 59665, 6915], 
-    '8':[35484, 41132, 50302], 
-    '9':[19085, 3481, 25375]}
+    '0':[4655], 
+    '1':[7748], 
+    '2':[1584], 
+    '3':[26364], 
+    '4':[26172], 
+    '5':[19175], 
+    '6':[58755], 
+    '7':[9724], 
+    '8':[35484], 
+    '9':[19085]}
 ideal_inds = np.array([*ideal.values()]).flatten().tolist()
 
 # for key in ideal.keys():
@@ -42,7 +53,7 @@ ideal_inds = np.array([*ideal.values()]).flatten().tolist()
 # TODO 1/2: Save this in your preferred format ..
 train_ideal = train_images[ideal_inds]
 
-file = open('ideal_train.txt', 'w')
+file = open('data/ideal_train.txt', 'w')
 for i in range(train_ideal.shape[0]):
     file.write(convert.convert_raw_vector(train_ideal[0]) + '\n')
 file.close()
@@ -67,7 +78,7 @@ test_images = pca.transform(test_images.reshape((-1, 784))).reshape((-1, 64))
 # TODO 2/2: ... and save this in your preferred format:
 train_ideal = train_images[ideal_inds]
 
-file = open('ideal_train_pca.txt', 'w')
+file = open('data/ideal_train_pca.txt', 'w')
 for i in range(train_ideal.shape[0]):
     file.write(convert.convert_raw_vector(train_ideal[0]) + '\n')
 file.close()
@@ -89,16 +100,23 @@ model.compile(
     optimizer='adam', loss='sparse_categorical_crossentropy',
     metrics=['accuracy'])
 
-
 model.fit(train_images, train_labels, epochs=10)
 
 # Prediction on 'ideals' after PCA:
 print(np.argmax(model.predict(train_ideal), axis=1))
+
+train_ideal_probs = model.predict(train_ideal)
+
+for i in range(train_ideal_probs.shape[0]):
+    file = open('data/ideal_train_probs_{label}.txt'.format(label=i), 'w')
+    file.write(convert.convert_raw_vector(train_ideal_probs[i]) + '\n')
+    file.close()
+
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 print('\nTest accuracy:', test_acc)
 model.summary()
 
-model.save('models/MNIST_PCA_64_ReLU_49_Softmax_10.h5')
+# model.save('models/MNIST_PCA_64_ReLU_49_Softmax_10.h5')
 
