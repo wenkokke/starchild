42a18ec1a60c65ace241853556632702a67e959f
Katya Komendantskaya <komendantskaya@gmail.com>
2020-03-16 10:15:48 +0000

    PCA 36 model

---
+++ train_MNIST_PCA_36_ReLU_25_Softmax_10.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle as pkl
+from sklearn.decomposition import PCA
+from tensorflow import keras
+
+# Load MNIST data
+mnist = keras.datasets.mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Scale inputs between [0,1]
+train_images = train_images.reshape((-1, 784)) / 255
+test_images = test_images.reshape((-1, 784)) / 255
+
+# Reduce dimensionality
+pca = PCA(n_components=36)
+train_images = pca.fit_transform(
+    train_images.reshape((-1, 784))).reshape((-1, 36))
+test_images = pca.transform(
+    test_images.reshape((-1, 784))).reshape((-1, 36))
+
+# Train network
+model = keras.Sequential([
+    keras.layers.Dense(25, activation='relu', input_shape=(36,)),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+# Save model
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('Test accuracy:', test_acc)
+model.summary()
+
+model.save('models/MNIST_PCA_36_ReLU_25_Softmax_10.h5')
+
+# Save inputs and outputs for ideal images
+ideal = {'0':4655 , '1':7748 , '2':1584, '3':26364, '4':26172,
+         '5':19175, '6':58755, '7':9724, '8':35484, '9':19085}
+
+ideal = {label: (ideal_in,
+                 model.predict(np.array([ideal_in]))[0])
+         for label, index in ideal.items()
+         for ideal_in in [train_images[index]]}
+
+with open('models/MNIST_PCA_36_ReLU_25_Softmax_10.ideal.pkl', 'wb') as fp:
+    pkl.dump(ideal, fp)
