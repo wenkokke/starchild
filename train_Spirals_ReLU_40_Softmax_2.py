7289ffe6628139d0706db27a11250f5e45b6ef20
leina0 <Daniel.Kienitz@web.de>
2020-04-25 15:56:03 +0100

    Added spiral experiment

---
+++ train_Spirals_ReLU_40_Softmax_2.py
@@ -0,0 +1,47 @@
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pickle as pkl
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+
+import convert
+
+    
+# Load spirals, spirals are approx. 0.5*pi awy from each other
+# x_data = np.load(file='D:/Github/starchild/x_data_7.5.npy')
+# y_data = np.load(file='D:/Github/starchild/y_data_7.5.npy')
+x_data = np.load(file='x_data_7.5.npy')
+y_data = np.load(file='y_data_7.5.npy')
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x_data, y_data, test_size=0.33)
+
+# Train network
+model = keras.Sequential([
+    keras.layers.Dense(40, activation='relu', input_shape=(2,)),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=50)
+
+# Save model
+train_loss, train_acc = model.evaluate(x_train, y_train, verbose=2)
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
+model.summary()
+print('Train accuracy:', train_acc)
+print('Test accuracy:', test_acc)
+
+
+model.save('models/Spirals_ReLU_40_Softmax_10.h5')
+
